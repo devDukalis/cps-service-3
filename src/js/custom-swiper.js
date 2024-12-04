87dd966e5c182a1dad5ba7d5fffef563db39d342
devdukalis <devdukalis@gmail.com>
2024-12-04 12:59:57 +0300

    feat: basic setup of swiper was added

---
+++ src/js/custom-swiper.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper(".swiper", {
+  // optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // sliding
+  slidesPerView: "auto",
+  spaceBetween: 16,
+
+  // pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
