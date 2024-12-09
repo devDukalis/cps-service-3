51c158efeab31c601b0556d68aa47758354d905d
devdukalis <devdukalis@gmail.com>
2024-12-09 15:47:49 +0300

    feat: toggle swiper slides and init swiper only on mobile devices features were added

--- src/js/custom-swiper.js
+++ src/js/custom-swiper.js
@@ -1,33 +1,53 @@
-const swiper = new Swiper(".swiper", {
-  // optional parameters
-  direction: "horizontal",
-  loop: false,
+/* INITIALIZE SWIPER ONLY ON MOBILE DEVICES */
+let swiper;
 
-  // sliding
-  slidesPerView: "auto",
-  // spaceBetween: 16,
+function isMobile() {
+  let screenWidth = window.innerWidth;
+  return screenWidth < 768;
+}
 
-  // pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+function initSwiperMobile() {
+  if (isMobile()) {
+    // Initialize swiper only if it wasn't initialized yet
+    if (!swiper) {
+      swiper = new Swiper(".swiper", {
+        // optional parameters
+        direction: "horizontal",
+        loop: false,
 
-  // navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+        // sliding
+        slidesPerView: "auto",
+        spaceBetween: 16, // Set default space between slides
 
-  // scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+        // pagination
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
 
-  // breakpoints
-  breakpoints: {
-    320: {
-      spaceBetween: 16,
-    },
-  },
-});
+        // navigation arrows
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+        // scrollbar
+        scrollbar: {
+          el: ".swiper-scrollbar",
+        },
+      });
+    }
+  } else {
+    // Destroy swiper if it exists
+    if (swiper) {
+      swiper.destroy();
+      swiper = null; // Reset swiper
+    }
+  }
+}
+
+// Init swiper on page load
+initSwiperMobile();
+
+// Add event listener for window resize
+window.addEventListener("resize", initSwiperMobile);
